47e9106bcca971e21ea5ae80679a09248e896a84
Steven Hill <steven@stevenhill.co.za>
2023-03-09 14:49:47 +0200

    Set window state management

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules
 const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,9 +8,20 @@ let mainWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
+  // config window state
+  // No need to set x and y coords because the window defaults to the center of the screen
+  let state = windowStateKeeper({ defaultWidth: 500, defaultHeight: 650 });
+
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    // Setting the state bounds (position) for the window here, for when the window position changes
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    // Setting maxWidth cos we don't want it to get too wide: list of links wont look good in a wide format
+    maxWidth: 650,
+    minHeight: 300,
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
@@ -21,6 +33,9 @@ function createWindow() {
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('renderer/main.html');
+
+  // Tell the window keeper state instance, which window to manage
+  state.manage(mainWindow);
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools();
